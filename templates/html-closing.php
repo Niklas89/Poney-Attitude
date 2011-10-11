164dea4816356b4c53d62675b66f9fe7a543bc75
Niklas Edelstam <niklas_edelstam@hotmail.com>
2011-10-11 10:38:38 +0200

    templates

---
+++ templates/html-closing.php
@@ -0,0 +1,12 @@
+﻿		</div> <!-- closing main -->
+		
+		<div id="bar">
+		</div> <!-- closing bar -->
+		
+		<div id="footer">
+		<p>Copyright 2011 Poney Attitude</p>
+		</div> <!-- closing footer -->
+		
+	</div> <!-- closing page -->
+</body>
+</html>
---
+++ templates/html-opening.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+	<head>
+	<title>Poney Attitude</title>
+		<link rel="stylesheet" type="text/css" href="templates/style.css" media="screen" />
+		<script type="text/javascript" src=""></script>
+		<meta name="author" content="Niklas Edelstam" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	</head>
+<body>
+	<div id="page">
+	
+		<div id="header">
+		</div> <!-- closing header -->
+		
+		<div id="bar">
+		</div> <!-- closing bar -->
+		
+		<div id="menu">
+		</div> <!-- closing menu -->
+
+		<div id="main">
